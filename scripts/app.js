98803b6fd3455e797ab0e193b5b1455c90aa5dd3
Magnus Kragelund <mak@ida.dk>
2024-03-09 23:52:53 +0100

    adds simple user features

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,20 @@
-var appInsights = window.appInsights || function (config) {
-    function s(config) { t[config] = function () { var i = arguments; t.queue.push(function () { t[config].apply(t, i) }) } }
-    var t = { config: config }, r = document, f = window, e = "script", n = r.createElement(e), i, u; for (n.src = config.url || "//az416426.vo.msecnd.net/scripts/a/ai.0.js", r.getElementsByTagName(e)[0].parentNode.appendChild(n), t.cookie = r.cookie, t.queue = [], i = ["Event", "Exception", "Metric", "PageView", "Trace", "Dependency"]; i.length;)s("track" + i.pop());
-    return config.disableExceptionTracking || (i = "onerror", s("_" + i), u = f[i], f[i] = function (config, r, f, e, n) { var s = u && u(config, r, f, e, n); return s !== !0 && t["_" + i](config, r, f, e, n), s }), t
-}({
-    instrumentationKey: "c3e291a5-1e64-42f9-aae0-1421afb145c4"
-});
+ function setUserNameClickListener (e) {
+    dataLayer.push({
+        event: "username_set",
+        username: document.getElementById("username").value
+    })
+ };
 
-let testCustomEvent = { 
-    name: "myCustomEvent", 
-    user_Id: "preben-01",
-    properties: { 
-        customProperty1: "customValue1", 
-        customProperty2: "customValue2" 
-    } 
-}
+function newsletterSignupClickListener (e) {
+    dataLayer.push({
+        event: "signed_up",
+        email: document.getElementById("email").value
+    })
+ };
 
-appInsights.trackEvent(testCustomEvent);
+ 
+
+ window.onload = function() {
+    document.getElementById("btnSetUsername").addEventListener("click", setUserNameClickListener);
+    document.getElementById("btnNewsletterSignup").addEventListener("click", newsletterSignupClickListener);
+ };
